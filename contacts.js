739ff8234b6135efa4bf6863078c800f26c0859a
lex <savchenko.lex@gmail.com>
2023-09-29 15:42:11 +0300

    list, getbyid, switch case, commander

--- contacts.js
+++ contacts.js
@@ -1,24 +1,90 @@
 const fs = require('fs/promises');
 const path = require('path');
+const {nanoid} = require('nanoid');
 
 const contactsPath = path.join(__dirname, '/db/contacts.json');
 
-function listContacts() {
+
+async function listContacts() {
     // ...твій код. Повертає масив контактів.
+    try {
+        const data = await fs.readFile(contactsPath, 'utf-8');
+        const allContacts = JSON.parse(data);
+        // console.log(allContacts);
+        return allContacts || null;
+        
+    } catch (error) {
+        console.error(error);
+    }
+    
   }
   
-  function getContactById(contactId) {
+  async function getContactById(contactId) {
     // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    if (!contactId) {
+        return "ID is required";
+    }
+
+    try {
+        const allContacts = await listContacts();
+
+        if (!allContacts) {
+            throw new error("A list of all contacts was not received.");
+        }
+
+        const selectedContact = allContacts.filter((contact)=>{ 
+            return contact.id === contactId;
+        });
+    
+        // console.log(selectedContact);
+        return selectedContact || null;
+
+    } catch (error) {
+        return error;
+    }
+
   }
   
-  function removeContact(contactId) {
+  async function removeContact(contactId) {
     // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+    if (!contactId) {
+        
+        return "ID is required";
+    }
+    
+    try {
+        const allContacts = await listContacts();
+        if (!allContacts) {
+            throw new error("A list of all contacts was not received.");
+        }
+        
+        const newVersion = allContacts.filter((contact)=>{ return contact.id !== contactId});
+        
+        console.log(newVersion);
+        return newVersion;
+
+    } catch (error) {
+        return error;
+    }
+
   }
   
-  function addContact(name, email, phone) {
+  async function addContact(name, email, phone) {
     // ...твій код. Повертає об'єкт доданого контакту. 
+    
+    const newContact = {
+        id: nanoid(),
+        name,
+        email, 
+        phone,
+    };
+
+    console.log(newContact);
   }
 
 module.exports = {
-    fs,
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
 }
--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-// const {fs, filepath} = require('./contacts.js');
+const {listContacts, getContactById, removeContact, addContact} = require('./contacts.js');
+const {program } = require('commander');
 
-console.log("node start ok");
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone')
+
+program.parse(process.argv);
+const options = program.opts();
+
+console.log("------------------ new cicle --------------------");
+// console.log(options);
+
+async function invokeAction({action, id, name, email, phone}) {
+    switch (action) {
+        case 'list':
+            console.log(typeof(action));
+            const list = await listContacts();
+            console.log(list);
+            break;
+        case "get":
+            getContactById(id); 
+            break;
+        case "add":
+            addContact(name, email, phone);
+            break;
+        case "remove":
+            removeContact(id);
+            break;    
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+            break;
+    }
+}
+
+invokeAction(options);
